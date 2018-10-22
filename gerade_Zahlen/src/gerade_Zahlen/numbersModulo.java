1e678d2b40598d5876a54596a7aeb37797665dfc
Jessi <Jessi@192.168.0.59>
2018-10-22 22:11:17 +0200

    for Schleife, die gerade Zahlen ausgibt

---
+++ gerade_Zahlen/src/gerade_Zahlen/numbersModulo.java
@@ -0,0 +1,15 @@
+package gerade_Zahlen;
+//import java.io.*;
+
+public class numbersModulo {
+	public static void main(String[] args) {
+		int i = 0;
+		for (i = 1; i <= 100; i++) {
+			if (i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+			while (i <= 100);
+	}
+
+}
